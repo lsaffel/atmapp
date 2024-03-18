6cae0986aba651fb13703cad3ca9d9d9c3c4a026
Lisa <llvvss@icloud.com>
2024-03-18 10:09:45 -0600

    show menu function

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# atmapp
+ATM app
+Initially shows the menu, below:
+
+1. Check balance
+2. Deposit
+3. Withdraw
--- src/atmapp.cpp
+++ src/atmapp.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-using namespace st;
+using namespace std;
+
+void showMenu();
 
 int main()
 {
-    
+    // check balance, deposit, withdraw, show menu
+    showMenu();
 
 return 0;
+}
+
+void showMenu() {
+    cout << "********** MENU **********" << endl;
+    cout << "1. Check balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "**************************" << endl;
+    
 }

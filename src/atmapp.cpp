358913c132e12f09805a8c8475b5c0b14d738bb2
Lisa <llvvss@icloud.com>
2024-03-18 10:28:48 -0600

    4 options and functions

--- src/atmapp.cpp
+++ src/atmapp.cpp
@@ -6,8 +6,30 @@ void showMenu();
 int main()
 {
     // check balance, deposit, withdraw, show menu
+    int option;
+    double balance = 500;
+    do {
     showMenu();
+    cout << "Please choose an option: ";
+    cin >> option;
 
+    switch (option) {
+        case 1: cout << "Balance is: " << balance << " $" << endl; break;
+        case 2: cout << "Deposit amount: ";
+            double depositAmount;
+            cin >> depositAmount;
+            balance += depositAmount;
+            break;
+        case 3: cout << "Withdraw amount: ";
+            double withdrawAmount;
+            cin >> withdrawAmount;
+            if(withdrawAmount <= balance)
+                balance -= withdrawAmount;
+            else
+                cout << "Your account does not have enough money to withdraw that amount." << endl;
+            break;
+    }
+    } while (option != 4);
 return 0;
 }
 
@@ -16,6 +38,7 @@ void showMenu() {
     cout << "1. Check balance" << endl;
     cout << "2. Deposit" << endl;
     cout << "3. Withdraw" << endl;
+    cout << "4. Exit" << endl;
     cout << "**************************" << endl;
     
 }
